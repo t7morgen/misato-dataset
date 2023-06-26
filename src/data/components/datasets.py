7d7c2c26c8d25b8301a84389aae7fd389ddaa8f4
Filipe Menezes <filipemmenezes@gmail.com>
2023-06-26 11:00:00 +0200

    updating license

--- src/data/components/datasets.py
+++ src/data/components/datasets.py
@@ -1,3 +1,24 @@
+'''MISATO, a database for protein-ligand interactions
+    Copyright (C) 2023  
+                        Till Siebenmorgen  (till.siebenmorgen@helmholtz-munich.de)
+                        Sabrina Benassou   (s.benassou@fz-juelich.de)
+                        Filipe Menezes     (filipe.menezes@helmholtz-munich.de)
+                        Erinç Merdivan     (erinc.merdivan@helmholtz-munich.de)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software 
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA'''
+
 from pathlib import Path
 import numpy as np
 import pandas as pd
